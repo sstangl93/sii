ecb95942b237707b4ace3020c284d0915f275678
sstangl93 <sstangl@robomatter.com>
2018-09-28 01:35:44 -0400

    Add files via upload

---
+++ js/scrollReveal.js
@@ -0,0 +1,55 @@
+window.sr = new ScrollReveal({ mobile: false });
+sr.reveal('.navbar .container', {
+	duration: 2000,
+	origin: 'left',
+	distance: '300px'
+});
+sr.reveal('.showcase-right', {
+	duration: 2000,
+	origin: 'right',
+	distance: '300px'
+});
+sr.reveal('.showcase-list', {
+	duration: 2000,
+	delay: 500,
+	origin: 'bottom',
+	distance: '300px'
+});
+sr.reveal('#industry .container', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('#technology .container', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('#build .container', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('#support .container', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('.largeIcon-sii', {
+	delay: 500,
+	duration: 1000,
+	origin: 'left',
+	distance: '200px',
+	viewFactor: 0.2
+});
+sr.reveal('.arrowText', {
+	delay: 1000,
+	duration: 1000,
+	origin: 'left',
+	distance: '200px',
+	viewFactor: 0.2
+});
